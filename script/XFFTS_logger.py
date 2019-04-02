7e61b5c4fc5164cf4bc83f75e3907889e2c675dc
Shota Ueda <ueda@s7u27m.local>
2019-04-02 18:30:55 +0900

    bug fix

--- script/XFFTS_logger.py
+++ script/XFFTS_logger.py
@@ -31,7 +31,7 @@ class logger(object):
         if self.log_flag:
             self.count += 1
             print(self.count)
-            self.op.write("BE{}".format(args["index"]), "", (req.data, time.time()), cur_num=args["index"]-1, auto_commit=True)
+            self.op.write("BE{}".format(args["index"]), "", (req.data, time.time()), cur_num=args["index"]-1, auto_commit=False)
         else: pass
         return
 
